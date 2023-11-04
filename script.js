c6a2172c364820f2bd27a7e5eccd2f5b146f6b7a
Marcelo Gomes <marcelog.3212@gmail.com>
2023-11-03 23:26:04 -0300

    add invite element

--- script.js
+++ script.js
@@ -15,4 +15,22 @@ button.onclick=()=>{
     }
 }
 
+function showInvite(){
+    let container = document.querySelector("#container");
+    let invitation = document.createElement('div');
+    invitation.classList.add('invitation');
+    container.appendChild(invitation);
+    invitation.innerHTML =`
+        <h2 style="font-family: 'VT323', monospace;font-size:30px; font-weight:bolder;text-shadow: 2px 2px black; color: white;"> LOREM </h1>
+        <p style="font-family: 'VT323', monospace;font-size:24px; font-weight:bolder; text-shadow: 2px 2px black; color: white;" >LOREM IPSUM dolor sit amet, consectetur adipisicing elit. Ducimus non, ipsam quaerat eligendi autem laborum commodi esse nemo quis accusantium nostrum earum fuga laboriosam! Voluptate amet consectetur nemo alias facilis.</p>
+        <br>
+        <p>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;👆<strong style="font-family: 'VT323', monospace;font-size:24px; font-weight:bolder; text-shadow: 2px 2px black; color: white;">Go Back</strong>👆</p>
+    `;
+    invitation.addEventListener('click',()=>{
+        invitation.classList.remove('invitation');
+        invitation.innerHTML="";
+    });
+
+}  
+
 
